42acf418ffc7c4fdd09e9635478a2e3b45e8f297
Gordon Ahn <tuxxon@nate.com>
2020-11-28 23:53:42 +0900

    first commit

---
+++ app/src/main/java/com/touchizen/bubblepicker/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.touchizen.bubblepicker
+
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.igalata.bubblepicker.BubblePickerListener
+import com.igalata.bubblepicker.adapter.BubblePickerAdapter
+import com.igalata.bubblepicker.model.BubbleGradient
+import com.igalata.bubblepicker.model.PickerItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val boldTypeface by lazy { Typeface.createFromAsset(assets, ROBOTO_BOLD) }
+    private val mediumTypeface by lazy { Typeface.createFromAsset(assets, ROBOTO_MEDIUM) }
+    private val regularTypeface by lazy { Typeface.createFromAsset(assets, ROBOTO_REGULAR) }
+
+    companion object {
+        private const val ROBOTO_BOLD = "roboto_bold.ttf"
+        private const val ROBOTO_MEDIUM = "roboto_medium.ttf"
+        private const val ROBOTO_REGULAR = "roboto_regular.ttf"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        titleTextView.typeface = mediumTypeface
+        subtitleTextView.typeface = boldTypeface
+        hintTextView.typeface = regularTypeface
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            subtitleTextView.letterSpacing = 0.06f
+            hintTextView.letterSpacing = 0.05f
+        }
+
+        val titles = resources.getStringArray(R.array.countries)
+        val colors = resources.obtainTypedArray(R.array.colors)
+        val images = resources.obtainTypedArray(R.array.images)
+
+        picker.adapter = object : BubblePickerAdapter {
+
+            override val totalCount = titles.size
+
+            override fun getItem(position: Int): PickerItem {
+                return PickerItem().apply {
+                    title = titles[position]
+                    gradient = BubbleGradient(colors.getColor((position * 2) % 8, 0),
+                        colors.getColor((position * 2) % 8 + 1, 0), BubbleGradient.VERTICAL)
+                    typeface = mediumTypeface
+                    textColor = ContextCompat.getColor(this@MainActivity, android.R.color.white)
+                    backgroundImage = ContextCompat.getDrawable(this@MainActivity, images.getResourceId(position, 0))
+                }
+            }
+        }
+
+        colors.recycle()
+        images.recycle()
+
+        picker.bubbleSize = 20
+        picker.listener = object : BubblePickerListener {
+            override fun onBubbleSelected(item: PickerItem) {
+                toast("${item.title} selected")
+            }
+
+            override fun onBubbleDeselected(item: PickerItem) {
+                toast("${item.title} deselected")
+            }
+
+            override fun onBubbleRemoved(item: PickerItem) {
+                toast("${item.title} removed")
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        picker.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        picker.onPause()
+    }
+
+    private fun toast(text: String) = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+}
---
+++ bubblepicker/src/main/java/com/igalata/bubblepicker/BubblePickerListener.kt
@@ -0,0 +1,16 @@
+package com.igalata.bubblepicker
+
+import com.igalata.bubblepicker.model.PickerItem
+
+/**
+ * Created by irinagalata on 3/6/17.
+ */
+interface BubblePickerListener {
+
+    fun onBubbleSelected(item: PickerItem)
+
+    fun onBubbleDeselected(item: PickerItem)
+
+    fun onBubbleRemoved(item: PickerItem)
+
+}
